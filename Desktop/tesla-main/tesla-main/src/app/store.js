3ed3ee0795261c14657006eb43e288501092b4e3
ggnadirashvili <ggnadirashvilii@gmail.com>
2023-01-16 16:19:00 +0400

    update

---
+++ Desktop/tesla-main/tesla-main/src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+// import carReducer from "../features/car/carSlice " // ar mushaobs
+
+export const store = configureStore({
+  reducer: {
+    // car: carReducer
+  },
+});   
